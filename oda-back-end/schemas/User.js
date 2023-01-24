cd48dbc7b5ce09a61786eb58538193bc033de089
seriojca <59750898+seriojca@users.noreply.github.com>
2023-01-24 22:33:36 +0200

    Update collection name

--- oda-back-end/schemas/User.js
+++ oda-back-end/schemas/User.js
@@ -4,7 +4,7 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   password: { type: String, required: true }
-})
+}, {collection: 'users'})
 
 const User = model('User', UserSchema);
 
